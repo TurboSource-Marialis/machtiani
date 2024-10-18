0eedd3308a35069ff7b0531d7358ddd8df632988
7db9a <noreply@yourdomain.com>
2024-10-18 13:47:54 -0400

    Introduce a new test case for the `status` command in the end-to-end tests to validate its functionality and improve test coverage.
    - Added `test_status_command.py` to implement the `status` command tests.
    - Integrated setup and teardown processes to manage the test environment effectively.
    - Included assertions to verify expected output when checking repository lock status.

---
+++ end-to-end-tests/test_status_command.py
@@ -0,0 +1,56 @@
+import unittest
+import time
+from test_utils.test_utils import (
+    clean_output,
+    run_machtiani_command,
+    Teardown,
+    Setup
+)
+
+class TestEndToEndMachtianiCommands(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Set the directory for the test
+        cls.directory = "data/git-projects/chastler"
+        # Initialize the Setup class with the git project directory
+        cls.setup = Setup(cls.directory)
+
+        # Fetch the latest branches
+        fetch_res = cls.setup.fetch_latest_branches()
+        print("Fetched branches:", fetch_res)
+        cls.setup.force_push("master-backup", "master")
+
+        branches = cls.setup.get_branches()
+        if "feature" not in branches:  # Checking for local branch existence
+            stdout, stderr = cls.setup.checkout_branch("feature")
+            print("Checkout Output:", stdout)
+            print("Checkout Errors:", stderr)
+        cls.setup.run_git_store()
+
+    def test_run_machtiani_status_command(self):
+        command = 'machtiani status'
+        stdout_machtiani, stderr_machtiani = run_machtiani_command(command, self.directory)
+        stdout_normalized = clean_output(stdout_machtiani)
+
+        expected_output = [
+            "Using remote URL: https://github.com/7db9a/chastler.git",
+            "The repo.lock file is not present."
+        ]
+
+        expected_output = [line.strip() for line in expected_output if line.strip()]
+        self.assertEqual(stdout_normalized, expected_output)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up the test environment by running the git delete command."""
+        try:
+            cls.teardown = Teardown(cls.directory)
+            stdout, stderr = cls.teardown.run_git_delete()
+            print("Teardown Output:", stdout)
+            print("Teardown Errors:", stderr)
+        except Exception as e:
+            print(f"Error during teardown: {e}")
+
+if __name__ == '__main__':
+    unittest.main()
