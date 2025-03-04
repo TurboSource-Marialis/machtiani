7467b05e90654ba2bd69a2a347bedf52421e46b6
7db9a <noreply@yourdomain.com>
2025-03-03 23:14:09 -0500

    feat: git-sync takes care of git-store and git-sync, depending if repo exists or not.

--- machtiani/internal/cli/git_sync.go
+++ machtiani/internal/cli/git_sync.go
@@ -2,6 +2,7 @@ package cli
 
 import (
     "fmt"
+    "strings"
 
     "github.com/7db9a/machtiani/internal/api"
     "github.com/7db9a/machtiani/internal/utils"
@@ -12,7 +13,21 @@ func handleGitSync(remoteURL, branchName string, apiKey *string, force bool, con
         return fmt.Errorf("Error: all flags --remote and --branch-name must be provided.")
     }
 
-    // Call the function to fetch and checkout the branch
+
+    _, err := api.CheckStatus(remoteURL, apiKey)
+    if err != nil {
+        if strings.Contains(err.Error(), "does not exist") {
+            // If the repository doesn't exist, add it
+            if err := addRepo(remoteURL, apiKey, force, config); err != nil {
+                return fmt.Errorf("Error adding repository: %w", err)
+            } else {
+                return nil
+            }
+        } else {
+            return fmt.Errorf("Error checking repository status: %w", err)
+        }
+    }
+
     message, err := api.FetchAndCheckoutBranch(remoteURL, remoteURL, branchName, apiKey, config.Environment.ModelAPIKey, force)
     if err != nil {
         return fmt.Errorf("Error syncing repository: %w", err)
@@ -20,5 +35,18 @@ func handleGitSync(remoteURL, branchName string, apiKey *string, force bool, con
 
     // Print the returned message
     fmt.Println(message)
+    return nil
+}
+
+func addRepo(remoteURL string, apiKey *string, force bool, config utils.Config) error {
+    response, err := api.AddRepository(remoteURL, remoteURL, apiKey, config.Environment.ModelAPIKey, api.RepoManagerURL, force)
+    if err != nil {
+        return fmt.Errorf("Error adding repository: %w", err)
+    }
+
+    fmt.Println(response.Message)
+    fmt.Println("---")
+    fmt.Println("Your repo is getting added to machtiani is in progress!")
+    fmt.Println("Please check back by running `machtiani status` to see if it completed.")
     return nil
 }
