597087e9db4d11e560ea71903e8b093aae8c0ac7
7db9a <noreply@yourdomain.com>
2025-04-24 20:32:03 -0400

    Fix path to variable for ld flags for compilation.

--- machtiani/generate_ldflags_local.go
+++ machtiani/generate_ldflags_local.go
@@ -22,7 +22,7 @@ func main() {
 	buildDate := time.Now().Format(time.RFC3339)
 
 	// Start constructing ldflags
-    ldflags := fmt.Sprintf("-X 'github.com/7db9a/machtiani/internal/api.HeadOID=%s' -X 'github.com/7db9a/machtiani/internal/api.BuildDate=%s' -X 'github.com/turboSource-marialis/machtiani/internal/api.MachtianiGitRemoteURL=%s' -X 'github.com/7db9a/machtiani/internal/cli.SystemMessageFrequencyHours=24'",
+    ldflags := fmt.Sprintf("-X 'github.com/7db9a/machtiani/internal/api.HeadOID=%s' -X 'github.com/7db9a/machtiani/internal/api.BuildDate=%s' -X 'github.com/7db9a/machtiani/internal/api.MachtianiGitRemoteURL=%s' -X 'github.com/7db9a/machtiani/internal/cli.SystemMessageFrequencyHours=24'",
     //ldflags := fmt.Sprintf("-X 'github.com/7db9a/machtiani/internal/api.HeadOID=%s' -X 'github.com/7db9a/machtiani/internal/api.BuildDate=%s' -X 'github.com/turboSource-marialis/machtiani/internal/api.MachtianiGitRemoteURL=%s' -X 'github.com/turboSource-marialis/machtiani/internal/cli.SystemMessageFrequencyHours=24'",
 		headOID,
 		buildDate,
